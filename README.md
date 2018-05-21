7a94250d7d3581b25453794485edf57a1ed41f0c
treey-yao <949091796@qq.com>
2018-05-21 11:54:59 +0800

    easy-mock的模拟数据和axios数据访问

--- README.md
+++ README.md
@@ -7,11 +7,28 @@
 Vue+Vuex+Router+Vant+Node+Koa2+Mongoose+MongoDB
 ```
 
+## 运行方式
+
+``` 
+# open project
+cd VueMall
+
+# install dependencies
+npm install
+
+# run
+ node run dev
+
+``` 
+
+
+
 
 ## 更新记录
 
 1. 2018/05/16 开坑
-1. 2018/05/18 了解Vant， 页面布局
+2. 2018/05/18 了解Vant， 页面布局
+3. 2018/05/21 easy-mock的模拟数据和axios数据访问
 
 
 
@@ -20,7 +37,7 @@ Vue+Vuex+Router+Vant+Node+Koa2+Mongoose+MongoDB
 #### 1. 2018/05/16
 开坑，动手这个项目， 搭建环境。 本项目是用 vue + koa2 开发所以我用的vue的脚手架Vue-cli。  
 
-#### 1. 2018/05/18
+#### 2. 2018/05/18
 安装Vant，了解Vant的基本操作。一下是注意事项：
 1. 在下载了Vant后，不建议全局，而使用 ` babel-plugin-import ` 安装。用那个模块装那个模块，这个会减少打包的大小，首页加载的时候会快一些。
 
@@ -28,7 +45,27 @@ Vue+Vuex+Router+Vant+Node+Koa2+Mongoose+MongoDB
 
 ps：VScode中 ` Vue VSCode Snippets `插件挺好用的
 
+#### 3. 2018/05/21
+1. easy-mock是比较好用的一个图形化的网站，我们完全可以使用他来模拟我们的Mock数据。
+网址：`` https://www.easy-mock.com/ ``
 
+2.在`created`的声明周期里取得数据 
+``` html
+created() {
+    axios({
+      url:"https://www.easy-mock.com/mock/5b022292676d1a45fe4e957b/VueMall/index",
+      method: "get"
+    })
+      .then(req => {
+        //请求成功后返回的参数
+        console.log(req);
+      })
+      .catch(err => {
+        //请求失败 报错
+        console.log(err);
+      });
+  }
+```
 
 
 > 革命未成功 同志需努力
--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,49 +1,67 @@
 <template>
-    <div class="">
-       <!--search -->
+  <div class="">
+    <!--search -->
     <div class="search-bar">
-        <van-row>
-            <van-col span="3"><img :src="locationIcon" class="location-icon" width="70%"/>
-                </van-col>
-            <van-col span="16">
-                <input type="text" class="search-input"/>
-            </van-col>
-            <van-col span="4"><van-button size="mini" class="search-btn">查找</van-button></van-col>
-        </van-row>
+      <van-row>
+        <van-col span="3"><img :src="locationIcon" class="location-icon" width="70%" />
+        </van-col>
+        <van-col span="16">
+          <input type="text" class="search-input" />
+        </van-col>
+        <van-col span="4">
+          <van-button size="mini" class="search-btn">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
+    <!-- 轮播图 -->
+    <div class="banner-box">
+      <van-swipe :autoplay="3000">
+        <van-swipe-item v-for="(banner, index) in bannerPicArray" :key="index">
+          <img v-lazy="banner.image" :id="banner.goodsId" width="100%" />
+        </van-swipe-item>
+      </van-swipe>
+    </div>
+    <!-- 分类栏 -->
+    <div class="type-tar">
+      <div v-for="(cate,index) in category" :key="index" class="type-items">
+        <img v-lazy="cate.image" width="90%" />
+        <span>{{cate.mallCategoryName}}</span>
+      </div>
+
     </div>
 
-    <div class="banner-box">
-    <!-- 轮播图 -->
-    <van-swipe :autoplay="3000">
-        <van-swipe-item  v-for="(banner, index) in bannerPicArray" :key="index">
-            <img v-lazy="banner.imageUrl"  width="100%"/>
-         </van-swipe-item>
-        </van-swipe>
-    </div>
-    
-    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       locationIcon: require("../../assets/images/location.png"),
-      bannerPicArray: [
-        {
-          imageUrl:
-            "http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg"
-        },
-        {
-          imageUrl:
-            "http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg"
-        },
-        {
-          imageUrl:
-            "http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg"
-        }
-      ]
+      bannerPicArray: [], //轮播图
+      category: []
     };
+  },
+  created() {
+    axios({
+      url:
+        "https://www.easy-mock.com/mock/5b022292676d1a45fe4e957b/VueMall/index",
+      method: "get"
+    })
+      .then(req => {
+        //请求成功后返回的参数
+        console.log(req);
+
+        if (req.status == 200) {
+          this.bannerPicArray = req.data.data.slides;
+          this.category = req.data.data.category;
+        }
+      })
+      .catch(err => {
+        //请求失败 报错
+        console.log(err);
+      });
   }
 };
 </script>
@@ -80,4 +98,23 @@ export default {
 .banner-box {
   max-height: 10rem;
 }
+/* 分类栏 */
+.type-tar {
+  background-color: #fff;
+  margin: 0 0.3rem 0.3rem 0.3rem;
+  border-radius: 0.3rem;
+  font-size: .7rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+
+.type-items {
+  padding:  0.5rem;
+  font-size: .7rem;
+  text-align: center;
+}
+
+
+
 </style>
